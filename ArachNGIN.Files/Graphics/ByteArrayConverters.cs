b735539fb962678f3bf7b03dfa5b510026f5d8f2
Michal Kuncl <michal.kuncl@gmail.com>
2013-05-07 11:30:00 +0200

    pridany prevodniky obrazku na bytove pole

---
+++ ArachNGIN.Files/Graphics/ByteArrayConverters.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Copyright (c) 2006-2013 Michal Kuncl <michal.kuncl@gmail.com> http://www.pavucina.info
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ArachNGIN.Files.Graphics
+{
+    /// <summary>
+    /// Převaděče obrázků na bytové pole a nazpátek
+    /// </summary>
+    public static class ByteArrayConverters
+    {
+        /// <summary>
+        /// Převede obrázek na bytové pole
+        /// </summary>
+        /// <param name="imageIn">vstupní obrázek</param>
+        /// <param name="imageFormat">formát výstupního obrázku</param>
+        /// <returns>bytové pole</returns>
+        public static byte[] ImageToByteArray(Image imageIn, ImageFormat imageFormat)
+        {
+            var ms = new MemoryStream();
+            imageIn.Save(ms, imageFormat);
+            return ms.ToArray();
+        }
+
+        /// <summary>
+        /// Převede bytové pole na obrázek
+        /// </summary>
+        /// <param name="byteArrayIn">bytové pole</param>
+        /// <returns>obrázek</returns>
+        public static Image ByteArrayToImage(byte[] byteArrayIn)
+        {
+            var ms = new MemoryStream(byteArrayIn);
+            var returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+    }
+}
