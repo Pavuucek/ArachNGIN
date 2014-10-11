5eee45a53f08859818376f3b0bcbcd21d8d46bf2
Michal Kuncl <michal.kuncl@gmail.com>
2014-10-11 22:17:48 +0200

    git-new-workdir.cmd tohle je lepsi na klonovani jedny vetve.

--- Clone-Documentation.cmd
+++ Clone-Documentation.cmd
@@ -1,9 +1,2 @@
 @echo off
-md ..\ArachNGIN_gh-pages >nul
-pushd ..\ArachNGIN_gh-pages
-git clone https://github.com/Pavuucek/ArachNGIN.git .
-git checkout origin/gh-pages -b gh-pages
-git branch -d master
-git branch
-popd
-pause
+call git-new-workdir.cmd . ..\ArachNGIN_gh-pages gh-pages
