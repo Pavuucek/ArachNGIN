eddd7c4374c7371b1c4fbff9e6834ee88ffd9571
Michal Kuncl <michal.kuncl@gmail.com>
2014-12-11 17:10:07 +0100

    ArachNGIN.Tests.Files project

---
+++ Tests/Files/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení jsou řízeny následujícím 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("ArachNGIN.Tests.Files")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArachNGIN.Tests.Files")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavením ComVisible na hodnotu false způsobí neviditelnost typů v rámci sestavení 
+// komponentám modelu COM.  Pokud je potřeba přistoupit k typu v rámci sestavení z 
+// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+// Následující GUID je použito pro ID knihovny typů, pokud je tento projekt vystaven COM
+[assembly: Guid("c78b9aa8-e0e2-4947-98e4-35f0684f7b7f")]
+
+// Informace o verzi sestavení se skládá z následujících čtyř hodnot:
+//
+//      Hlavní verze
+//      Dílčí verze 
+//      Číslo sestavení
+//      Revize
+//
+// Lze zadat všechny hodnoty nebo lze použít výchozí sestavení a čísla revize 
+// použitím '*' jak je ukázáno dále:
+// [sestavení: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
