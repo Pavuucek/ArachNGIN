8fa0e273afd591718e3d91974752a69c22685d5c
Michal Kuncl <michal.kuncl@gmail.com>
2014-12-11 17:18:20 +0100

    AnimeCrcTests initial import

---
+++ Tests/Files/CRC/AnimeCrcTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ArachNGIN.Files.CRC;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ArachNGIN.Files.CRC.Tests
+{
+    [TestClass()]
+    public class AnimeCrcTests
+    {
+        [TestMethod()]
+        public void CalculateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCrcFromStreamUintTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCrcFromStreamTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCrcFromFileUintTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCrcFromFileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ResetCrcTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest2()
+        {
+            Assert.Fail();
+        }
+    }
+}
