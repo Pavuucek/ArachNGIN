aa2cdf24060af06d0d6057f7b3e5611a47252e5b
Michal Kuncl <michal.kuncl@gmail.com>
2016-11-24 14:00:25 +0100

    add TracerPublisher class

---
+++ Tracer/ITracerHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArachNGIN.Tracer
+{
+    public interface ITracerHandler
+    {
+        void Trace(TracerMessage tracerMessage);
+    }
+}
---
+++ Tracer/TracerPublisher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArachNGIN.Tracer
+{
+    internal class TracerPublisher
+    {
+        private IList<TracerMessage> _TracerMessages;
+        private IList<ITracerHandler> _TracerHandlers;
+
+        public TracerPublisher()
+        {
+            _TracerMessages = new List<TracerMessage>();
+            _TracerHandlers = new List<ITracerHandler>();
+        }
+
+        public void Trace(TracerMessage tracerMessage)
+        {
+            _TracerMessages.Add(tracerMessage);
+            foreach (var tracerHandler in _TracerHandlers)
+            {
+                tracerHandler.Trace(tracerMessage);
+            }
+        }
+
+        public IEnumerable<TracerMessage> Messages
+        {
+            get { return _TracerMessages; }
+        }
+    }
+}
