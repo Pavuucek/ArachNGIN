fbfae24f1ca0b52e777016149d77dd40f0787573
Michal Kuncl <michal.kuncl@gmail.com>
2016-11-24 14:08:15 +0100

    add output handlers

---
+++ Tracer/Handlers/ConsoleHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArachNGIN.Tracer.Handlers
+{
+    public class ConsoleHandler : ITracerHandler
+    {
+        public void Trace(TracerMessage tracerMessage)
+        {
+            Console.WriteLine(tracerMessage.ToString());
+        }
+    }
+}
---
+++ Tracer/Handlers/DebugHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArachNGIN.Tracer.Handlers
+{
+    public class DebugHandler : ITracerHandler
+    {
+        public void Trace(TracerMessage tracerMessage)
+        {
+            System.Diagnostics.Debug.WriteLine(tracerMessage.ToString());
+        }
+    }
+}
