02ca77f8a69b0055c53ad6ce7d575ae2d68268db
Michal Kuncl <michal.kuncl@gmail.com>
2016-12-17 16:42:23 +0100

    CommandLine: parameters test

---
+++ ArachNGIN.CommandLine.Tests/ParametersTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+using System.Collections;
+
+namespace ArachNGIN.CommandLine.Tests
+{
+    [TestClass]
+    public class ParametersTest
+    {
+        private string[] testParameters = { "action", "/debug", "--size=100", "/color:blue" };
+
+        [TestMethod]
+        public void TestParameters()
+        {
+            var args = new Parameters(testParameters);
+            args["debug"].ShouldBe("true");
+            args["size"].ShouldBe("100");
+            args["color"].ShouldBe("blue");
+            args["action"].ShouldBe("true");
+        }
+    }
+}
