0523bbb9a95c7fe98fcca081c0a72fa3289d58a6
Michal Kuncl <michal.kuncl@gmail.com>
2016-12-27 12:29:56 +0100

    Ini: Add tests

---
+++ Tests/Files/Settings/IniTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArachNGIN.Tests.Files.Settings
+{
+    [TestClass]
+    public class IniTests
+    {
+        [TestMethod]
+        public void WriteAndReadString()
+        {
+        }
+    }
+}
