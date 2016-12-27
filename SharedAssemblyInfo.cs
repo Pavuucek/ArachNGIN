182f058f37f4c5d697a75c01199e21bf8447b4b7
Michal Kuncl <michal.kuncl@gmail.com>
2016-12-27 21:37:53 +0100

    Simplify projet AssemblyInfo.cs files, introduce SharedAssemblyInfo.cs in solution root

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//[assembly: AssemblyTitle("ArachNGIN.Files")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("X-C Soft ltd.")]
+[assembly: AssemblyProduct("ArachNGIN")]
+[assembly: AssemblyCopyright("Copyright © X-C Soft ltd. 2007 - 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.1.131")]
+[assembly: AssemblyVersion("1.0.1.131")]
+[assembly: AssemblyFileVersion("1.0.1.131")]
+[assembly: AssemblyInformationalVersion("Optimization:1.0.1-131-g7b03d9c")]
