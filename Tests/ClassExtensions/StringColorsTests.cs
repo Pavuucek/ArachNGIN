7f5a8c0c19016853d28482579ea894e4c08ac839
Michal Kuncl <michal.kuncl@gmail.com>
2016-12-27 11:24:29 +0100

    Tests: add StringColorTests

---
+++ Tests/ClassExtensions/StringColorsTests.cs
@@ -0,0 +1,28 @@
+﻿using ArachNGIN.ClassExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace ArachNGIN.Tests.ClassExtensions
+{
+    [TestClass]
+    public class StringColorsTests
+    {
+        [TestMethod]
+        public void ColorToNamedStringAndBack()
+        {
+            foreach (KnownColor knownColor in Enum.GetValues(typeof(KnownColor)))
+            {
+                var s = Color.FromKnownColor(knownColor).NamedColorToString();
+                Console.Write(s);
+                var c = s.NamedColorStringToColor();
+                Console.Write(" => ");
+                Console.Write(c.ToString());
+                Console.WriteLine();
+            }
+        }
+    }
+}
