1e7e62a5d5fbcbe8616ef69d6a28899d5af14721
Michal Kuncl <michal.kuncl@gmail.com>
2016-12-27 13:44:33 +0100

    StringColors: FromString default to black

--- ClassExtensions/StringColors.cs
+++ ClassExtensions/StringColors.cs
@@ -23,7 +23,14 @@ namespace ArachNGIN.ClassExtensions
             if (Enum.TryParse(strColorName, out knownColor))
                 return Color.FromKnownColor(knownColor);
 
-            return ColorTranslator.FromHtml("#" + strColorName);
+            try
+            {
+                return ColorTranslator.FromHtml("#" + strColorName);
+            }
+            catch
+            {
+                return Color.Black;
+            }
         }
     }
 }
