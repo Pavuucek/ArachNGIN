822f7360fed72dfe5cf16c034e3caf10626392b7
Michal Kuncl <michal.kuncl@gmail.com>
2016-12-28 14:22:31 +0100

    Tests: add String tests

---
+++ Tests/ClassExtensions/StringsTests.cs
@@ -0,0 +1,26 @@
+﻿using ArachNGIN.ClassExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace ArachNGIN.Tests.ClassExtensions
+{
+    [TestClass]
+    public class StringsTests
+    {
+        [TestMethod]
+        public void TestAppendString()
+        {
+            var s = "this";
+            s = s.Append("string");
+            s.ShouldBe("thisstring");
+        }
+
+        [TestMethod]
+        public void TestAppendChar()
+        {
+            var s = "ab";
+            s = s.Append('c').Append('d');
+            s.ShouldBe("abcd");
+        }
+    }
+}
